0387cd887dfdcaaf0d5a411ff0ad927a612060b4
Kamil Danecki <danecki.kamil@gmail.com>
2025-01-15 17:51:50 +0100

    update cmake and cargo dependencies and added build instructions
    Signed-off-by: Kamil Danecki <danecki.kamil@gmail.com>

--- server/build.rs
+++ server/build.rs
@@ -1,9 +1,23 @@
-fn main() {
-    println!(r"cargo:rustc-link-search=../core/build/");
-    println!(r"cargo:rustc-link-lib=pime");
-    println!(r"cargo:rustc-link-arg=-Wl,-rpath,../core/build/");
+use cmake::Config;
+use std::env;
+use std::path::PathBuf;
 
-    /*cc::Build::new()
-        .file("../c_part/clib.c")
-        .compile("foo");*/
+fn main() {
+    let dst = Config::new("../core").build_target("pime").build();
+
+    println!("cargo:rustc-link-search=native={}/build/", dst.display());
+    println!("cargo:rustc-link-lib=dylib=pime");
+
+    let bindings = bindgen::Builder::default()
+        .header("core_headers.h")
+        .clang_arg("-xc++")
+        .clang_arg("-std=c++14")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("core_bindings.rs");
+    bindings
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
 }
